15ca817e0b41c73a8f8dee493f004280f38ce311
Alanx789 <sonic600@live.com.mx>
2022-10-28 17:14:34 -0500

    V4

--- controllers/setOrder.js
+++ controllers/setOrder.js
@@ -3,13 +3,44 @@ const db = {FB}
 
 const setOrder = async(req, res) => {
     try {
+        //Conseguir el objeto solicitado
         const {body: {collection_name, amount, collection_price}} = req
+
+        //Conseguir ID
         const {params: {id}} = req
 
+        //Obtener datos del usuario
         const users = db.collection('Cerditos').doc(id)
         const {_fieldsProto: {money, collectibles} }= await users.get()
 
-        const new 
+        //Dinero restante 
+        const price = money - (amount * collection_price);
+        
+        //Variables en función del precio 
+        const businessE = (price < 0) ? [] : ["INSUFICIENT_BALANCE"]
+        const newCollectibles = (price < 0) ? [...collectibles] : [...collectibles, {
+            "collection_name": collection_name,
+            "amount": amount,
+            "collection_price": collection_price
+        }]
+        const currentBalance = (price < 0) ? money : price
+
+        //Actualizar info 
+        const update = await users.update({
+            id: id,
+            money : currentBalance,
+            collectibles: newCollectibles
+        })
+
+        //Respuesta a Postman
+        res.send({
+            "current balance": {
+                "money" : currentBalance,
+                "collectibles": newCollectibles
+            },
+            "business errors" : businessE
+
+        })
 
 
 
